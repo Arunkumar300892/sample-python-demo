c063839771219fdceef1c73e2ec4eb8207c7a4f4
Arunkumar Ranaganathan <arunkumar300892@gmail.com>
2018-10-16 18:07:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 import sys
-
 from datetime import datetime
 from simple_salesforce import Salesforce 
 sf = Salesforce(username='abatini27@gmail.com', password='Salesa@1234', security_token='L9hbrqxdutQWm1bmswnM8uN51', sandbox=False)

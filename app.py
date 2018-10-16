f0be5567fbff645d2e1d0c651b60fca246f50cc6
Arunkumar Ranaganathan <arunkumar300892@gmail.com>
2018-10-16 18:06:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
-print("I am learning Python !")
-exit()
+import os
+import sys
+
+from datetime import datetime
+from simple_salesforce import Salesforce 
+sf = Salesforce(username='abatini27@gmail.com', password='Salesa@1234', security_token='L9hbrqxdutQWm1bmswnM8uN51', sandbox=False)
+Lastname='AD Bathini'
+Firstname='Bathini'
+Accountcreation = sf.Account.create({'Name':Lastname})

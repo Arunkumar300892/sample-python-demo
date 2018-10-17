1d836bca1ae959ef7e408ae9b4bf8b5b81bfbe08
Arunkumar Ranaganathan <arunkumar300892@gmail.com>
2018-10-17 10:46:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ from simple_salesforce import Salesforce
 sf = Salesforce(username='abatini27@gmail.com', password='Salesa@1234', security_token='4x8sp5Hi9tlbcBzfOCv2cP8N', sandbox=False)
 Lastname='Ranganathan'
 Firstname='Arunkumar'
-Name = Firstname + Lastname
+Name = Firstname + " " + Lastname
 print("User Name:" + Name)
 Accountcreation = sf.Account.create({'Name': Name})
-print("AccountCreated:" + Accountcreation)
+print("AccountCreated:")
+for k, v in Accountcreation.items():
+    print k, v
 exit()
